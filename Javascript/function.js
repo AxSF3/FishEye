ce54fb44f1ed5a25f43c2e4b76c63dd3fba77c0c
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2021-09-10 22:10:38 +0200

    Delete function.js

--- Javascript/function.js
+++
@@ -1,7 +0,0 @@
-import { ELEMENTHTML} from "./constant.js";
-
-
-export const showAllTags = () => {
-  ELEMENTHTML.worker.forEach((item) => (item.style.display = "initial")); /* FONCTION POUR AFFICHER LES TAGS */
-};
-
