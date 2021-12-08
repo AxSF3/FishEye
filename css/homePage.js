d68e4fd00342db8cacff8095a3b52bb12b75b673
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2021-12-08 02:14:13 +0100

    Add files via upload

---
+++ css/homePage.js
@@ -0,0 +1,42 @@
+import PhotographerList from './photographerList.js';
+import Header from './header.js';
+
+export default class HomePage {
+
+  static getData() {
+    return fetch('data.json') /* Appeler l'API Fetch */
+    .then(response => response.json()) /*  Convertir res en données JSON */
+    .then(data => { /* Traiter les données JSON */
+      HomePage.data;
+      return data;
+    })
+
+  }
+  
+  
+
+  static init() {
+    const dataPromise = HomePage.getData();
+
+    window.onload = () => {
+
+      dataPromise.then(data => {
+        const photographers = data.photographers;
+        const header = document.querySelector('.header-index');
+        const mainTitle = document.querySelector('.main-title');
+        const sectionHomepage = document.querySelector('#section-homepage');
+    
+        const photographerList = new PhotographerList(sectionHomepage, photographers);
+        const headerContent = new Header(header, photographerList);
+        headerContent.createHeader();
+        mainTitle.appendChild(document.createTextNode('Nos photographes'));
+       
+        photographerList.displayRelevantCards();
+        sectionHomepage.setAttribute('tabindex', '5');
+        sectionHomepage.setAttribute('aria-label', 'cartes des photographes');
+      });
+      
+    };
+  }
+}
+
---
+++ css/mediaFactory.js
@@ -0,0 +1,143 @@
+export default class MediaFactory {
+  static createMedia(mediaData) {
+   
+    if (mediaData.hasOwnProperty('image')) return new Image(mediaData.image, mediaData.photographerId,/* mediaData.price,*/ mediaData.likes, mediaData.id, mediaData.desc);
+    else if (mediaData.hasOwnProperty('video')) return new Video(mediaData.video, mediaData.photographerId,/* mediaData.price,*/ mediaData.likes, mediaData.id, mediaData.desc);
+    else {
+      console.log(mediaData);
+    }
+  }
+
+}
+
+export class Video {
+  constructor(fileName, photographerId, /*price,*/ likes, id, desc) {
+    this.fileName = fileName;
+    this.photographerId = photographerId;
+    /*this.price = price;*/
+    this.likes = likes;
+    this.titleContent = this.fileName.slice(0, this.fileName.length-4).replaceAll('_', ' ');
+    this.id = id;
+    this.desc = desc;
+
+  }
+
+  // Création élément DOM média de la galerie
+  createGalleryDom() {
+    const cardVideo = document.createElement('figure');
+    const mediaVideo = document.createElement('video');
+    const mediaVideoSrc = document.createElement('source');
+    const mediaCardInfo = document.createElement('figcaption');
+    const mediaCardInfoText = document.createElement('div');
+    const mediaCardInfoHeart = document.createElement('div');
+    const heartNumber = document.createElement('div');
+    const heart = document.createElement('i');
+    const title = document.createElement('div');
+    /*const price = document.createElement('div');*/
+    
+    mediaVideo.classList.add('media-video');
+    mediaVideo.setAttribute('tabindex', '0');
+    mediaVideo.setAttribute('title', `${this.desc}`);
+    mediaVideo.setAttribute('controls', 'controls');
+    mediaVideo.setAttribute('muted', 'muted');
+
+    mediaCardInfo.classList.add('media-card_info');
+    mediaCardInfoText.classList.add('media-card_info__text');
+
+    mediaCardInfoHeart.classList.add('media-card_info__heart');
+    mediaCardInfoHeart.setAttribute('tabindex', '0');
+    mediaCardInfoHeart.setAttribute('aria-label', `${this.likes} likes cliquez pour ajouter un like`);
+
+    heartNumber.classList.add('heart-number');
+    heart.classList.add('fas', 'fa-heart');
+    heart.setAttribute('title', 'likes');
+
+    title.classList.add('title');
+    /*price.classList.add('price');*/
+
+    title.appendChild(document.createTextNode(`${this.titleContent}`));
+    /*price.appendChild(document.createTextNode(`${this.price}€`));*/
+    mediaVideoSrc.src = `resources/${this.photographerId}/Resized_images/${this.fileName}`;
+    heartNumber.innerHTML = this.likes;
+
+    mediaVideo.appendChild(mediaVideoSrc);
+    mediaCardInfo.appendChild(mediaCardInfoText);
+    mediaCardInfoText.append(title/*, price*/);
+    mediaCardInfoHeart.append(heartNumber,heart);
+    mediaCardInfo.appendChild(mediaCardInfoHeart);
+
+    cardVideo.append(mediaVideo, mediaCardInfo);
+
+    cardVideo.dataset['mediaTitle'] = this.titleContent;
+
+    return cardVideo;
+  }
+
+
+
+}
+
+
+
+export class Image {
+  constructor(fileName, photographerId, /*price,*/ likes, id, desc) {
+  
+    this.fileName = fileName;
+    this.photographerId = photographerId;
+    /*this.price = price;*/
+    this.likes = likes;
+    this.titleContent = this.fileName.slice(0, this.fileName.length-4).replaceAll('_', ' ');
+    this.id = id;
+    this.desc = desc;
+    
+  }
+
+  // Création élément DOM média de la galerie
+  createGalleryDom() {
+    const cardImage = document.createElement('figure');
+    const mediaImage = document.createElement('img');
+    const mediaCardInfo = document.createElement('figcaption');
+    const mediaCardInfoText = document.createElement('div');
+    const mediaCardInfoHeart = document.createElement('div');
+    const heartNumber = document.createElement('div');
+    const heart = document.createElement('i');
+    const title = document.createElement('div');
+    /*const price = document.createElement('div');*/
+
+    mediaImage.setAttribute('tabindex', '0');
+    mediaImage.setAttribute('alt', `${this.desc}`);
+
+    mediaCardInfo.classList.add('media-card_info');
+    mediaCardInfoText.classList.add('media-card_info__text');
+
+    mediaCardInfoHeart.classList.add('media-card_info__heart');
+    mediaCardInfoHeart.setAttribute('tabindex', '0');
+    mediaCardInfoHeart.setAttribute('aria-label', `${this.likes} likes cliquez pour ajouter un like`);
+
+    heartNumber.classList.add('heart-number');
+    heart.classList.add('fas', 'fa-heart');
+    heart.setAttribute('title', 'likes');
+
+    title.classList.add('title');
+    /*price.classList.add('price');*/
+
+    title.appendChild(document.createTextNode(`${this.titleContent}`));
+   /* price.appendChild(document.createTextNode(`${this.price}€`));*/
+
+    mediaImage.src = `resources/${this.photographerId}/Resized_images/${this.fileName}`;
+    heartNumber.innerHTML = this.likes;
+
+    mediaCardInfo.appendChild(mediaCardInfoText);
+    mediaCardInfoText.append(title/*, price*/);
+    mediaCardInfoHeart.append(heartNumber, heart);
+    mediaCardInfo.appendChild(mediaCardInfoHeart);
+
+    cardImage.append(mediaImage, mediaCardInfo);
+
+    cardImage.dataset['mediaTitle'] = this.titleContent;
+
+    return cardImage;
+  }
+
+
+}
